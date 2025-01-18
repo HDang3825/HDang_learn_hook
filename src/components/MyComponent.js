cf49393466938e55e16da0153f8d5838cab068f2
Vo Le Hai Dang <910volehaidang2019@gmail.com>
2025-01-18 16:43:43 +0700

    by Hai Dang

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const MyComponent = (props) => {
+
+    return (
+        <>
+            <div>
+                <input type="text" />
+                <button type="submit">Thêm</button>
+            </div>
+        </>
+    )
+}
+export default MyComponent;
